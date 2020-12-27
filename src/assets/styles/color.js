6bbaf373b169654d8e8c98b2d0918f2aaffeef85
Lyndsay Ramberg <rambergl@yahoo.com>
2020-12-27 10:25:15 -0800

    change autofill background color

--- src/assets/styles/color.js
+++ src/assets/styles/color.js
@@ -5,6 +5,7 @@ const ICE_BLUE = '#EDFBFF';
 const LIGHT_PINK = '#FFD4DE';
 const RED = '#FF8C8C';
 const LIGHT_GRAY = '#CDCBD1';
+const PURPLE = '#211742';
 
 export const color = {
   WHITE,
@@ -13,5 +14,6 @@ export const color = {
   ICE_BLUE,
   LIGHT_PINK,
   RED,
-  LIGHT_GRAY
+  LIGHT_GRAY,
+  PURPLE
 }
