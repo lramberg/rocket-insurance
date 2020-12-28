da1fb77e2d893de3a12070b31e677c68a757963d
Lyndsay Ramberg <rambergl@yahoo.com>
2020-12-27 16:30:02 -0800

    font color and remove unused color

--- src/assets/styles/color.js
+++ src/assets/styles/color.js
@@ -2,12 +2,10 @@ const DEEP_PURPLE = '#0D021A';
 const ICE_BLUE = '#EDFBFF';
 const RED = '#FF8C8C';
 const BLUE_GRAY = '#BFD9FF';
-const DEEP_BLUE = '#10172b';
 
 export const color = {
   DEEP_PURPLE,
   ICE_BLUE,
   RED,
-  BLUE_GRAY,
-  DEEP_BLUE
+  BLUE_GRAY
 }
